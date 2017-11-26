08c90b93c3827f4b28a5c19f9bb596ee8c318c8a
MorinFred <morin.frederick@gmail.com>
2017-11-26 11:46:15 -0500

    Fred 26/11
    Autres pages qui font rien encore

--- RaspCloud/api/routes/pageRouting.js
+++ RaspCloud/api/routes/pageRouting.js
@@ -16,9 +16,33 @@ module.exports = (app) => {
         }
     });
 
+    app.route('/admin').get((req,res)=>{
+        if(!req.session.user){
+            res.redirect('/');
+        }else{
+            res.render('admin');
+        }
+    });
+
+    app.route('/profil').get((req,res)=>{
+        if(!req.session.user){
+            res.redirect('/');
+        }else{
+            res.render('profil', { firstName: req.session.user.firstName ,lastName: req.session.user.lastName, type_user: req.session.user.fk_type_user});
+        }
+    });
+
     app.route('/logout').get((req,res)=>{
         req.session.destroy();
         console.log('Logging out...');
         res.redirect('/');
+    });
+
+    app.route('/adduser').get((req,res)=>{
+        if(!req.session.user){
+            res.redirect('/');
+        }else{
+            res.render('adduser');
+        }
     });
 };
