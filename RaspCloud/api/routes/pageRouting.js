fe007d473c94a271ce63852432c99f8e6cee6f8b
MorinFred <morin.frederick@gmail.com>
2017-11-27 21:21:22 -0500

    Fred 27/11
    Bugs

--- RaspCloud/api/routes/pageRouting.js
+++ RaspCloud/api/routes/pageRouting.js
@@ -32,20 +32,11 @@ module.exports = (app) => {
     app.route('/profil').get((req,res)=>{
         if(!req.session.user){
             res.redirect('/');
-<<<<<<< HEAD
-        }else {
-            res.render('profil', {
-                firstName: req.session.user.firstName,
-                lastName: req.session.user.lastName,
-                type_user: req.session.user.fk_type_user
-            });
-=======
         }else{
             if (req.query.error !== null) {
                 res.render('profil', { firstName: req.session.user.firstName, lastName: req.session.user.lastName, type_user: req.session.user.fk_type_user, error: req.query.error});
             }
             res.render('profil', { firstName: req.session.user.firstName, lastName: req.session.user.lastName, type_user: req.session.user.fk_type_user});
->>>>>>> fred
         }
     });
 
